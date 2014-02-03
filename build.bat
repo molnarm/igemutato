987dd06b9603a961284295fbe1a171a4df126ffe
Molnár Márton <molnar.marton@gmail.com>
2014-02-03 21:33:15 +0100

    Automatikus .crx build

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo JS minify
+java -jar ..\tools\yuicompressor-2.4.8.jar igemutato.js -o chrome\igemutato.min.js -v
+
+echo.
+echo CSS minify
+java -jar ..\tools\yuicompressor-2.4.8.jar igemutato.css -o chrome\igemutato.min.css -v
+
+echo.
+echo Chrome packaging
+%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe --pack-extension=%1\chrome --pack-extension-key=%1\igemutato.pem
+
+echo Done.
--- igemutato.js
+++ igemutato.js
@@ -1,10 +1,7 @@
 //var Szentiras=function(){function m(b){var c,d,e,f,g;if(b=b&&b.firstChild)do if(d=b.nextSibling,e=b.parentNode,1===b.nodeType&&-1==config.excludeTags.indexOf(b.nodeName.toLowerCase()+","))m(b);else if(3===b.nodeType){for(g=b.data;c=a.exec(g);)f=n(c),f&&(e.insertBefore(j.createTextNode(RegExp.leftContext),b),e.insertBefore(f,b),g=RegExp.rightContext,a.lastIndex=0);e.insertBefore(j.createTextNode(g),b),e.removeChild(b)}while(b=d)}function n(a){var b=j.createElement("a");return b.className+=" ige-link",b.appendChild(j.createTextNode(a[0])),b.addEventListener("mouseover",function(a){g=setTimeout(function(){d&&d.parentNode.removeChild(d),q(a)},config.tipShow)},!1),b.addEventListener("mouseout",function(){d&&(h=setTimeout(function(){!g&&d&&(d.parentNode.removeChild(d),d=null)},config.tipHide)),g&&(clearTimeout(g),g=null)}),b}function o(a,b){var c=new XMLHttpRequest;return"withCredentials"in c?c.open(a,b,!0):"undefined"!=typeof XDomainRequest?(c=new XDomainRequest,c.open(a,b)):c=null,c}function p(a){i&&i.abort(),i=o("GET",b+encodeURI(a.innerText)+"&forditas="+config.forditas),i.onreadystatechange=function(){if(d&&4===i.readyState&&200===i.status){var a=JSON.parse(i.responseText),b="";if(a&&a.valasz&&a.valasz.versek&&a.valasz.versek.length){for(var c=0;c<a.valasz.versek.length;c++)b+=a.valasz.versek[c].szoveg+" ";e.innerText=b}else e.innerText="A betöltés sikertelen :-("}},i.send()}function q(a){var b=a.target||a.srcElement;p(b),d||(d=j.createElement("div"),e=j.createElement("div"),e.className+="szoveg",d.appendChild(e),f=j.createElement("div"),f.className+="igehely",d.appendChild(f)),d.id="igemutato",d.addEventListener("mouseover",function(){h&&(clearTimeout(h),h=null)},!1),d.addEventListener("mouseout",function(){h=setTimeout(function(){!g&&d&&(d.parentNode.removeChild(d),d=null)},config.tipHide)}),f.innerHTML='&nbsp;<a href="'+c+config.forditas+"/"+encodeURI(b.innerText.replace(/\s/g,""))+'"><b>'+b.innerText+"</b>&nbsp;(szentiras.hu)&nbsp;&raquo;</a>",e.innerText="Betöltés...";var i,m,n,o,q=b.getBoundingClientRect();i=q.top+(k.scrollTop||l.scrollTop),m=q.left+(k.scrollLeft||l.scrollLeft),n=b.offsetHeight,o=l.clientWidth||window.innerWidth,d.style.top=(q.top>config.tipH+config.tipD?i-config.tipH-config.tipD:i+n+config.tipD)+"px",d.style.left=(m+config.tipW>o?o-config.tipW-config.tipD:m)+"px",d.style.width=config.tipW+"px",d.style.height=config.tipH+"px",e.style.height=config.tipH-30+"px",l.appendChild(d)}var a=/([12](?:K(?:[io]r|rón)|Makk?|Pé?t(?:er)?|Sám|T(?:h?essz?|im))|[1-3]Já?n(?:os)?|[1-5]Móz(?:es)?|(?:Ap)Csel|A(?:gg?|bd)|Ám(?:ós)?|B(?:ár|[ií]r(?:ák)?|ölcs)|Dán|É(?:sa|zs|n(?:ek(?:ek|Én)?)?)|E(?:f(?:éz)?|szt?|z(?:s?dr?)?)|Fil(?:em)?|Gal|H(?:a[bg]|ós)|Iz|J(?:ak|á?n(?:os)?|e[lr]|o(?:el)?|ó(?:[bn]|zs|el)|[Ss]ir(?:alm?)?|úd(?:ás)?|ud(?:it)?)|K(?:iv|ol)|L(?:ev|u?k(?:ács)?)|M(?:al(?:ak)?|á?té?|(?:ár)?k|ik|Törv)|N[áe]h|(?:Ó|O)z|P(?:él|ré)d|R(?:óm|[uú]th?)|S(?:ir(?:alm?)?|ír|z?of|zám)|T(?:er|it|ób)|Z(?:ak|of|s(?:olt|id)?))(?: ?[0-9]+)(?:[;,.\- ]*[0-9]+[a-z]?)*/g,b="http://szentiras.hu/API/?feladat=idezet&hivatkozas=",c="http://szentiras.hu/";config={forditas:"SZIT",tipW:300,tipH:200,tipD:10,tipShow:200,tipHide:500,excludeTags:"head,script,input,select,textarea,h1,h2,h3,a,"};var d,e,f,g,h,i,j=document,k=j.documentElement,l=j.body;return{config:config,keres:m}}();
 var Szentiras = (function() {
-	var regexp = /([12](?:K(?:[io]r|rón)|Makk?|Pé?t(?:er)?|Sám|T(?:h?essz?|im))|[1-3]Já?n(?:os)?|[1-5]Móz(?:es)?|(?:Ap)Csel|A(?:gg?|bd)|Ám(?:ós)?|B(?:ár|[ií]r(?:ák)?|ölcs)|Dán|É(?:sa|zs|n(?:ek(?:ek|Én)?)?)|E(?:f(?:éz)?|szt?|z(?:s?dr?)?)|Fil(?:em)?|Gal|H(?:a[bg]|ós)|Iz|J(?:ak|á?n(?:os)?|e[lr]|o(?:el)?|ó(?:[bn]|zs|el)|[Ss]ir(?:alm?)?|úd(?:ás)?|ud(?:it)?)|K(?:iv|ol)|L(?:ev|u?k(?:ács)?)|M(?:al(?:ak)?|á?té?|(?:ár)?k|ik|Törv)|N[áe]h|(?:Ó|O)z|P(?:él|ré)d|R(?:óm|[uú]th?)|S(?:ir(?:alm?)?|ír|z?of|zám)|T(?:er|it|ób)|Z(?:ak|of|s(?:olt|id)?))(?: ?[0-9]+)(?:[;,.\- ]*[0-9]+[a-z]?)*/g;
-	// API URL
-	var api = 'http://szentiras.hu/API/?feladat=idezet&hivatkozas=';
-	// szöveg
-	var olvasas ='http://szentiras.hu/';
+	
+	// beállítások, kívülről felülírható
 	config = {
 		// fordítás
 		forditas: 'SZIT',
@@ -21,10 +18,15 @@ var Szentiras = (function() {
 		// kizárt tagek
 		excludeTags : ",head,script,input,select,textarea,h1,h2,h3,a,"
 	};
-
-	var tooltip, szoveg, igehely, linkTimeout, tipTimeout, xmlhttp;
-	var d = document, e = d.documentElement, b = d.body;
-
+	
+	var regexp = /([12](?:K(?:[io]r|rón)|Makk?|Pé?t(?:er)?|Sám|T(?:h?essz?|im))|[1-3]Já?n(?:os)?|[1-5]Móz(?:es)?|(?:Ap)Csel|A(?:gg?|bd)|Ám(?:ós)?|B(?:ár|[ií]r(?:ák)?|ölcs)|Dán|É(?:sa|zs|n(?:ek(?:ek|Én)?)?)|E(?:f(?:éz)?|szt?|z(?:s?dr?)?)|Fil(?:em)?|Gal|H(?:a[bg]|ós)|Iz|J(?:ak|á?n(?:os)?|e[lr]|o(?:el)?|ó(?:[bn]|zs|el)|[Ss]ir(?:alm?)?|úd(?:ás)?|ud(?:it)?)|K(?:iv|ol)|L(?:ev|u?k(?:ács)?)|M(?:al(?:ak)?|á?té?|(?:ár)?k|ik|Törv)|N[áe]h|(?:Ó|O)z|P(?:él|ré)d|R(?:óm|[uú]th?)|S(?:ir(?:alm?)?|ír|z?of|zám)|T(?:er|it|ób)|Z(?:ak|of|s(?:olt|id)?))(?: ?[0-9]+)(?:[;,.\- ]*[0-9]+[a-z]?)*/g,
+	// API URL
+	api = 'http://szentiras.hu/API/?feladat=idezet&hivatkozas=',
+	// szöveg
+	olvasas ='http://szentiras.hu/',
+	
+	tooltip, szoveg, igehely, linkTimeout, tipTimeout, xmlhttp, d = document, e = d.documentElement, b = d.body;
+	
 	function keres(node) {
 		var match, next, parent, replacementNode, text;
 
@@ -131,12 +133,18 @@ var Szentiras = (function() {
 	}
 
 	function showTooltip(event) {
-		var a = event.target || event.srcElement;
+		var a = event.target || event.srcElement, offsetTop, offsetLeft, triggerH, screenW, r = a.getBoundingClientRect();
+		
 		ajax(a);
+		
 		tooltip || (tooltip = d.createElement('div'),
 				szoveg = d.createElement('div'), szoveg.className += 'szoveg', tooltip.appendChild(szoveg),
 				igehely = d.createElement('div'), igehely.className += 'igehely', tooltip.appendChild(igehely)
 		);
+
+		igehely.innerHTML = '&nbsp;<a href="' + olvasas + config.forditas + '/' + encodeURI(a.innerText.replace(/\s/g, "")) + '"><b>'+ a.innerText + '</b>&nbsp;(szentiras.hu)&nbsp;&raquo;</a>';
+		szoveg.innerText = "Betöltés...";
+		
 		tooltip.id = "igemutato";
 		tooltip.addEventListener("mouseover", function() {
 			// amíg a tooltipen van az egér, addig marad megjelenítve
@@ -155,12 +163,7 @@ var Szentiras = (function() {
 				}
 			}, config.tipHide);
 		});
-
-		igehely.innerHTML = '&nbsp;<a href="' + olvasas + config.forditas + '/' + encodeURI(a.innerText.replace(/\s/g, "")) + '"><b>'+ a.innerText + '</b>&nbsp;(szentiras.hu)&nbsp;&raquo;</a>';
-		szoveg.innerText = "Betöltés...";
-
-		var offsetTop, offsetLeft, triggerH, screenW;
-		var r = a.getBoundingClientRect();
+		
 		offsetTop = r.top + (e.scrollTop || b.scrollTop);
 		offsetLeft = r.left + (e.scrollLeft || b.scrollLeft);
 		triggerH = a.offsetHeight;
