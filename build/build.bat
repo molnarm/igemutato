6315a638e1207aac3ff9c4da53af3235c61ee371
Molnár Márton <molnar.marton@gmail.com>
2014-02-07 20:18:47 +0100

    Javított build fájlok

--- build/build.bat
+++ build/build.bat
@@ -8,4 +8,12 @@ echo CSS minify
 java -jar ..\tools\yuicompressor-2.4.8.jar igemutato.css -o igemutato.min.css -v
 
 echo.
+echo Copying files
+copy igemutato.min.js "chrome/igemutato.min.js" /y
+copy igemutato.min.css "chrome/igemutato.min.css" /y
+
+copy igemutato.min.js "firefox/data/igemutato.min.js" /y
+copy igemutato.min.css "firefox/data/igemutato.min.css" /y
+
+echo.
 echo Done.
--- build/chrome.bat
+++ build/chrome.bat
@@ -1,9 +1,5 @@
 @echo off
 
-echo Copying files
-copy igemutato.min.js "chrome/igemutato.min.js" /y
-copy igemutato.min.css "chrome/igemutato.min.css" /y
-
 echo Chrome packaging
 %LOCALAPPDATA%\Google\Chrome\Application\chrome.exe --pack-extension=%1\chrome --pack-extension-key=%1\igemutato.pem
 
