512ade22ea6d9c0fc6792a98cd6ca96fbd4ec885
Molnár Márton <molnar.marton@gmail.com>
2017-04-07 22:31:17 +0200

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'); 
+
+var cleanCss = require('gulp-clean-css');
+
+//gulp.task('minify-css', function () {
+//    return gulp.src('styles/*.css')
+//        .pipe(cleanCSS({ compatibility: 'ie8' }))
+//        .pipe(gulp.dest('dist'));
+//});
