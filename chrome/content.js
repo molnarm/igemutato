80d2714cbed03d24c14a6cf87f5450d1a54037e5
Molnár Márton <molnar.marton@gmail.com>
2017-04-15 15:16:59 +0200

    Valami rejtélyes okból ez a fájl nem volt rendesen trackelve...

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,5 @@
 chrome.storage.sync.get('config', function(result) {
-	for (key in result.config) {
-		Szentiras.config[key] = result.config[key];
-	}
-	Szentiras.keres(document.body);
+    if(result && result.config)	
+        Szentiras.setConfig(result.config);
+    Szentiras.start(document.body);
 });
