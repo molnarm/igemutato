0e211bbf227123b6157e2c2630055d53a7c5f969
Molnár Márton <molnar.marton@gmail.com>
2017-04-18 20:17:31 +0200

    Chrome build gulppal (egyelőre a meglévő könyvtárszerkezettel)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,69 @@
-var gulp = require('gulp'); 
+/// <binding AfterBuild="build" Clean="clean" />
 
-var cleanCss = require('gulp-clean-css');
+const gulp = require("gulp");
 
-//gulp.task('minify-css', function () {
-//    return gulp.src('styles/*.css')
-//        .pipe(cleanCSS({ compatibility: 'ie8' }))
-//        .pipe(gulp.dest('dist'));
-//});
+const uglify = require("gulp-uglify");
+const cleanCss = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const del = require("del");
+const run = require("gulp-run");
+var fs = require("fs");
+const crx = require("gulp-crx-pack");
+
+const debug = require("gulp-debug");
+
+// COMMON TASKS
+
+const extensionsDir = "extensions";
+
+gulp.task("minify-css", function () {
+    return gulp.src("igemutato.css")
+        .pipe(cleanCss({ compatibility: "ie8" }))
+        .pipe(rename("igemutato.min.css"))
+        .pipe(gulp.dest(extensionsDir));
+});
+
+gulp.task("clean", ["clean-chrome"]);
+gulp.task("build", ["build-chrome"/*, "firefox", "web", "wordpress"*/]);
+
+// CHROME
+
+const chromeDir = "chrome";
+
+gulp.task("clean-chrome", function () {
+    return del([
+        "chrome.crx"
+        , chromeDir + "/igemutato.js"
+        , chromeDir + "/igemutato.min.js"
+        , chromeDir + "/igemutato.min.css"
+        , extensionsDir + "/chrome.crx"
+    ]);
+});
+
+gulp.task("transform-js-chrome", function (callback) {
+    return run("powershell -ExecutionPolicy Bypass -File build/stripregions.ps1 igemutato.js " + chromeDir + "/igemutato.js CHROME").exec();
+});
+
+gulp.task("minify-js-chrome", ["transform-js-chrome"], function () {
+    return gulp.src(chromeDir + "/igemutato.js")
+        .pipe(uglify())
+        .pipe(rename("igemutato.min.js"))
+        .pipe(gulp.dest(chromeDir));
+});
+
+gulp.task("prepare-chrome", ["minify-css", "minify-js-chrome"], function () {
+    del.sync([chromeDir + "/igemutato.js"]);
+    return gulp.src(extensionsDir + "/igemutato.min.css")
+        .pipe(gulp.dest(chromeDir));
+});
+
+gulp.task("package-chrome", ["prepare-chrome"], function () {
+    return gulp.src(chromeDir)
+        .pipe(crx({
+            privateKey: fs.readFileSync(extensionsDir + "/igemutato.pem", "utf8"),
+            filename: "chrome.crx"
+        }))
+        .pipe(gulp.dest(extensionsDir));
+});
+
+gulp.task("build-chrome", ["package-chrome"]);
