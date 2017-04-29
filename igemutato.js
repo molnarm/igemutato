ae0fec6743113810edd1d7b00f4a44ad0e54cb63
Molnár Márton <molnar.marton@gmail.com>
2017-04-29 15:07:52 +0200

    #20. Chrome-ban a select megnyitásakor két mouseout event jön egymás után: az elsőnél a select a relatedtarget, a másodiknál null, a selecten belül mozogva már nincs újabb. Firefoxban és IE-ben a nullos event később, a selecten belül mozogva érkezik csak (ez okozza a hibát), Edge-ben egyáltalán nincs ilyen.
    Talán hasonló észrevétel ez is: https://github.com/jquery/jquery/issues/3558

--- igemutato.js
+++ igemutato.js
@@ -23,25 +23,25 @@ var Szentiras = (function () {
         showNumbers: false
     },
 
-    regexp = /(?:^|[^\w\u00C0-\u017F])((?:[12](?:K(?:[io]r|rón)|Makk?|Pé?t(?:er)?|Sám|T(?:h?essz?|im))|[1-3]Já?n(?:os)?|[1-5]Móz(?:es)?|(?:Ap)?Csel|A(?:gg?|bd)|Ám(?:ós)?|B(?:ár|[ií]r(?:ák)?|ölcs)|Dán|É(?:sa|zs|n(?:ek(?:ek|Én)?)?)|E(?:f(?:éz)?|szt?|z(?:s?dr?)?)|Fil(?:em)?|Gal|H(?:a[bg]|ós)|Iz|J(?:ak|á?n(?:os)?|e[lr]|o(?:el)?|ó(?:[bn]|zs|el)|[Ss]ir(?:alm?)?|úd(?:ás)?|ud(?:it)?)|K(?:iv|ol)|L(?:ev|u?k(?:ács)?)|M(?:al(?:ak)?|á?té?|(?:ár)?k|ik|Törv)|N[áe]h|(?:Ó|O)z|P(?:él|ré)d|R(?:óm|[uú]th?)|S(?:ir(?:alm?)?|ír|z?of|zám)|T(?:er|it|ób)|Z(?:ak|of|s(?:olt|id)?))\.?(?:\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?(?:\s*[-–—]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?)?(?:\s*[\|;]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?(?:\s*[-–—]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?)?)*))(?:(?=[^\w\u00C0-\u017F])|$)/g,
-    forditasok = ['KNB', 'SZIT', 'KG', 'UF', 'RUF', 'BD'],
-    // API URL
-    url = (window.location.protocol == 'https:' ? 'https:' : 'http:') + '//szentiras.hu/',
-    api = url + 'api/idezet/',
-    // tooltip elemei
-    tooltip, szoveg, igehely, forditasSelect,
-    // timeoutok
-    linkTimeout, tipTimeout,
-    // lekérdezések kellékei
-    xmlhttp, cache = {},
-    // aktuális adatok
-    forditas, ige,
-    // DOM elemek
-    d = document, b = d.body, e = d.documentElement,
-    // kizárt elemek
-    excludes,
-    // facepalm
-    ie8;
+        regexp = /(?:^|[^\w\u00C0-\u017F])((?:[12](?:K(?:[io]r|rón)|Makk?|Pé?t(?:er)?|Sám|T(?:h?essz?|im))|[1-3]Já?n(?:os)?|[1-5]Móz(?:es)?|(?:Ap)?Csel|A(?:gg?|bd)|Ám(?:ós)?|B(?:ár|[ií]r(?:ák)?|ölcs)|Dán|É(?:sa|zs|n(?:ek(?:ek|Én)?)?)|E(?:f(?:éz)?|szt?|z(?:s?dr?)?)|Fil(?:em)?|Gal|H(?:a[bg]|ós)|Iz|J(?:ak|á?n(?:os)?|e[lr]|o(?:el)?|ó(?:[bn]|zs|el)|[Ss]ir(?:alm?)?|úd(?:ás)?|ud(?:it)?)|K(?:iv|ol)|L(?:ev|u?k(?:ács)?)|M(?:al(?:ak)?|á?té?|(?:ár)?k|ik|Törv)|N[áe]h|(?:Ó|O)z|P(?:él|ré)d|R(?:óm|[uú]th?)|S(?:ir(?:alm?)?|ír|z?of|zám)|T(?:er|it|ób)|Z(?:ak|of|s(?:olt|id)?))\.?(?:\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?(?:\s*[-–—]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?)?(?:\s*[\|;]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?(?:\s*[-–—]\s*[0-9]{1,3}(?:[,:]\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?(?:\.\s*[0-9]{1,2}[a-z]?(?:\s*[-–—]\s*[0-9]{1,2}[a-z]?\b(?![,:]))?)*)?)?)*))(?:(?=[^\w\u00C0-\u017F])|$)/g,
+        forditasok = ['KNB', 'SZIT', 'KG', 'UF', 'RUF', 'BD'],
+        // API URL
+        url = (window.location.protocol == 'https:' ? 'https:' : 'http:') + '//szentiras.hu/',
+        api = url + 'api/idezet/',
+        // tooltip elemei
+        tooltip, szoveg, igehely, forditasSelect,
+        // timeoutok
+        linkTimeout, tipTimeout,
+        // lekérdezések kellékei
+        xmlhttp, cache = {},
+        // aktuális adatok
+        forditas, ige,
+        // DOM elemek
+        d = document, b = d.body, e = d.documentElement,
+        // kizárt elemek
+        excludes,
+        // facepalm
+        ie8;
 
     // #if !CHROME
     // #if !FIREFOX
@@ -331,6 +331,9 @@ var Szentiras = (function () {
         };
         // ha elvisszük róla az egeret, akkor elrejtjük
         tooltip.onmouseout = function (event) {
+            var e = event.toElement || event.relatedTarget;
+            if (!e) return;
+
             clearTimeout(tipTimeout);
             tipTimeout = setTimeout(function () {
                 hideTooltip();
