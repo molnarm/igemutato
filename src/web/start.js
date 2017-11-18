392b46751b407d3c5bc7d49ae8dffb0744cdc90c
Molnár Márton <molnar.marton@gmail.com>
2017-11-18 15:06:55 +0100

    CSS should be loaded over HTTPS to avoid mixed content errors

--- src/web/start.js
+++ src/web/start.js
@@ -1,7 +1,7 @@
 var css = document.createElement("link");
 css.setAttribute("rel", "stylesheet");
 css.setAttribute("type", "text/css");
-css.setAttribute("href", 'http://molnarm.github.io/igemutato.min.css');
+css.setAttribute("href", 'https://molnarm.github.io/igemutato.min.css');
 document.getElementsByTagName("head")[0].appendChild(css);
 
 window.igemutato && window.igemutato.config && Szentiras.setConfig(window.igemutato.config);
